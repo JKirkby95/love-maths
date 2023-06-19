b1983c752339277771fc394477e1516ec65d582a
JKirkby95 <>
2023-06-19 17:50:36 +0000

    add increment score and wrong answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,8 +39,10 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
     if (isCorrect){
         alert("Hey you got it right! :D")
+        incrementScore();
     } else {
         alert(`Awww.. you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -59,11 +61,19 @@ function calculateCorrectAnswer(){
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
+/**
+ * gets current score from the dom and increments it by 1 for correct answer
+ */
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
+/**
+ * gets current incorrect score from the dom and increments it by 1 for correct answer
+ */
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 function displayAdditionQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
